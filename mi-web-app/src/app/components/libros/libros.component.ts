f6d65cc2a7d6a1289bbaa45259b2cbb72d4b56b9
Orbp1403 <2492944860101@ingenieria.usac.edu.gt>
2021-12-01 21:44:45 -0600

    Usando formularios para agregar libros

--- mi-web-app/src/app/components/libros/libros.component.ts
+++ mi-web-app/src/app/components/libros/libros.component.ts
@@ -8,11 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class LibrosComponent implements OnInit {
   libros = ['Matematica 1', 'Algoritmos Basico', 'Algebra Nivel Basico'];
 
+
   constructor() {}
 
   ngOnInit(): void {}
 
   eliminarLibro(libro) {
     this.libros = this.libros.filter((p) => p !== libro);
+  }
+
+  guardarLibro(formulario){
+    if(formulario.valid){
+      this.libros.push(formulario.value.nombreLibro)
+    }
   }
 }
